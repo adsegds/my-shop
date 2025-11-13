4ccebaa492097f9d316c0b51a91a9e290e20d6e9
adsegds <142857317+adsegds@users.noreply.github.com>
2025-11-13 15:24:17 +0900

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,13 @@
-// products.js - 商品データ
+// ================================
+// 商品データベース
+// ================================
 const PRODUCTS = [
+    // ---- ペットボトル飲料 (cat:1) ----
     {
         id: 1,
-        name: "クラフトボス甘くないイタリアーノ (500ml)",
+        name: "クラフトボス 甘くないイタリアーノ (500ml)",
         price: 258,
-        image: "/my-shop/img/boss.jpg",
+        image: "./img/boss.jpg",
         stock: 10,
         cat: 1
     },
@@ -12,7 +15,7 @@ const PRODUCTS = [
         id: 2,
         name: "コカ・コーラ (500ml)",
         price: 248,
-        image: "/my-shop/img/coke.jpg",
+        image: "./img/coke.jpg",
         stock: 20,
         cat: 1
     },
@@ -20,15 +23,17 @@ const PRODUCTS = [
         id: 3,
         name: "サントリー 天然水 (2L)",
         price: 188,
-        image: "/my-shop/img/water.jpg",
+        image: "./img/water.jpg",
         stock: 15,
         cat: 1
     },
+
+    // ---- お菓子系 (cat:2) ----
     {
         id: 4,
-        name: "プリングルズ<サワークリーム&オニオン> (26g)",
+        name: "プリングルズ サワークリーム&オニオン (26g)",
         price: 398,
-        image: "/my-shop/img/pringles.jpg",
+        image: "./img/pringles.jpg",
         stock: 8,
         cat: 2
     },
@@ -36,7 +41,7 @@ const PRODUCTS = [
         id: 5,
         name: "金のとりから からあげくん",
         price: 88,
-        image: "/my-shop/img/torikara.jpg",
+        image: "./img/torikara.jpg",
         stock: 12,
         cat: 2
     },
@@ -44,12 +49,28 @@ const PRODUCTS = [
         id: 6,
         name: "LW こんがり焼しそチップス (47g)",
         price: 178,
-        image: "/my-shop/img/chips.jpg",
+        image: "./img/chips.jpg",
         stock: 0,
         cat: 2
     }
 ];
 
+// ================================
+// カテゴリ別取得（キャッシュ付き）
+// ================================
+const PRODUCT_CACHE = {};
+
 function getProductsByCat(cat) {
-    return PRODUCTS.filter(p => p.cat === cat);
+    if (!PRODUCT_CACHE[cat]) {
+        PRODUCT_CACHE[cat] = PRODUCTS.filter(p => p.cat === cat);
+    }
+    return PRODUCT_CACHE[cat];
 }
+
+// ================================
+// 商品をIDから取得
+// ================================
+function getProductById(id) {
+    return PRODUCTS.find(p => p.id === id) || null;
+}
+
