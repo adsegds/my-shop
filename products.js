393f2b1bfee2a323af54b6d51fcd26e7a7edc509
adsegds <142857317+adsegds@users.noreply.github.com>
2025-11-13 14:04:57 +0900

    Update products.js

--- products.js
+++ products.js
@@ -1,53 +1,52 @@
 // products.js - 商品データ
-
 const PRODUCTS = [
-    { 
-        id: 1, 
-        name: "クラフトボス甘くないイタリアーノ (500ml)", 
-        price: 258, 
-        image: "/my-shop/img/boss.jpg", 
-        stock: 10, 
-        cat: 1 
+    {
+        id: 1,
+        name: "クラフトボス甘くないイタリアーノ (500ml)",
+        price: 258,
+        image: "/my-shop/img/boss.jpg",
+        stock: 10,
+        cat: 1
     },
-    { 
-        id: 2, 
-        name: "コカ・コーラ (500ml)", 
-        price: 248, 
-        image: "/my-shop/img/coke.jpg", 
-        stock: 20, 
-        cat: 1 
+    {
+        id: 2,
+        name: "コカ・コーラ (500ml)",
+        price: 248,
+        image: "/my-shop/img/coke.jpg",
+        stock: 20,
+        cat: 1
     },
-    { 
-        id: 3, 
-        name: "サントリー 天然水 (2L)", 
-        price: 188, 
-        image: "/my-shop/img/water.jpg", 
-        stock: 15, 
-        cat: 1 
+    {
+        id: 3,
+        name: "サントリー 天然水 (2L)",
+        price: 188,
+        image: "/my-shop/img/water.jpg",
+        stock: 15,
+        cat: 1
     },
-    { 
-        id: 4, 
-        name: "プリングルズ<サワークリーム&オニオン> (26g)", 
-        price: 398, 
-        image: "/my-shop/img/pringles.jpg", 
-        stock: 8, 
-        cat: 2 
+    {
+        id: 4,
+        name: "プリングルズ<サワークリーム&オニオン> (26g)",
+        price: 398,
+        image: "/my-shop/img/pringles.jpg",
+        stock: 8,
+        cat: 2
     },
-    { 
-        id: 5, 
-        name: "金のとりから からあげくん", 
-        price: 88, 
-        image: "/my-shop/img/torikara.jpg", 
-        stock: 12, 
-        cat: 2 
+    {
+        id: 5,
+        name: "金のとりから からあげくん",
+        price: 88,
+        image: "/my-shop/img/torikara.jpg",
+        stock: 12,
+        cat: 2
     },
-    { 
-        id: 6, 
-        name: "LW こんがり焼しそチップス (47g)", 
-        price: 178, 
-        image: "/my-shop/img/chips.jpg", 
-        stock: 0, 
-        cat: 2 
+    {
+        id: 6,
+        name: "LW こんがり焼しそチップス (47g)",
+        price: 178,
+        image: "/my-shop/img/chips.jpg",
+        stock: 0,
+        cat: 2
     }
 ];
 
